18b25ec1ac8ece156d778c1f38626b206020aada
lucasrosengarten <rosenglu112900@cvh.rusd.us>
2017-02-03 10:31:57 -0800

    half way

--- Loops.py
+++ Loops.py
@@ -1,9 +1,12 @@
 """Write a program that will add 5 and loop until it reaches a number GREATER
 than 100.  It should then spit out the result AND tell the user how many times
 it had to add 5 (if any)"""
-
-
-
+count = 0
+while count < 100:
+   print (count, " is  less than 100")
+   count = count + 5
+else:
+   print (count, " is not less than 100")
 
 
 
