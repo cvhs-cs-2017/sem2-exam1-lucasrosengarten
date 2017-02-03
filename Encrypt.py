d04e85389eca60bc76fc7af8c0efb48e021b493a
lucasrosengarten <rosenglu112900@cvh.rusd.us>
2017-02-03 10:46:22 -0800

    half way

--- Encrypt.py
+++ Encrypt.py
@@ -3,8 +3,24 @@
 'Computer Science Makes the World go round but it doesn't make the world round itself!'
 
 Print the save the result as the variable = NoVowels
-""""
+"""
+def novowels(anystring):
+    newstring = ""
+    for ch in anystring:
+        a = ord(ch)
+        a = a + 5
+        newstring = newstring + chr(a)
+    return newstring
+print (novowels ("Cmptr Scnc Mks th Wrld g rnd bt t dsn't mk th wrld rnd tslf!"))
 
+def decrypt(anystring):
+    newstring = ""
+    for ch in anystring:
+        a = ord(ch)
+        a = a - 5
+        newstring = newstring + chr(a)
+    return newstring
+print (decrypt(novowels("Cmptr Scnc Mks th Wrld g rnd bt t dsn't mk th wrld rnd tslf"))
 
 
 
